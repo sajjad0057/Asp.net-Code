b0dadf19ff7226776b7b4f9dee27e1b84a9142c6
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-18 17:54:07 +0600

    Practice QueryExecution using Linq

---
+++ FeaturePractice/Linq_Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using Try101LinqSamples;
+
+public class Program
+{
+    public static void Main(string[] agrs)
+    {
+
+        #region QueryExecution
+        QueryExecution queryExecution = new QueryExecution();
+
+        queryExecution.DeferredExecution();
+        Console.WriteLine("--------------------------------------");
+        queryExecution.EagerExecution();
+        Console.WriteLine("--------------------------------------");
+        queryExecution.ReuseQueryDefinition();
+
+        #endregion
+    }
+}
