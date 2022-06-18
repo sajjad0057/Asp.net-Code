4a8da10ab295e7d74aef774dd63f284aa7b97523
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-18 23:11:09 +0600

    Practice delegate using basic syntax

---
+++ FeaturePractice/DelegatesAndEvent_Practice/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Delegate
+{
+    public class Program
+    {
+        public delegate void PrintDelegate(object message);
+
+        public static void Calculate(int x, int y, PrintDelegate printer)
+        {
+            int result = x + y;
+            printer(result);
+        }
+    }
+
+
+    public class Test
+    {
+        public static void Print1(object message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Print2(object message)
+        {
+            Console.WriteLine($"-----{message}------");
+        }
+
+        public static void Main(string[] args)
+        {
+            Program.Calculate(1, 3, Print1);
+            Program.Calculate(2, 3, Print2);
+        }
+    }
+}
+
