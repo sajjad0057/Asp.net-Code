0da072bd0e20fb5fa1bb51fdcf08bcafed000be1
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-18 18:14:35 +0600

    Practice Data Projections using Linq syntax

--- FeaturePractice/Linq_Practice/Program.cs
+++ FeaturePractice/Linq_Practice/Program.cs
@@ -8,14 +8,42 @@ public class Program
     {
 
         #region QueryExecution
-        QueryExecution queryExecution = new QueryExecution();
 
-        queryExecution.DeferredExecution();
-        Console.WriteLine("--------------------------------------");
-        queryExecution.EagerExecution();
-        Console.WriteLine("--------------------------------------");
-        queryExecution.ReuseQueryDefinition();
+        //QueryExecution queryExecution = new QueryExecution();
+        //queryExecution.DeferredExecution();
+        //Console.WriteLine("--------------------------------------");
+        //queryExecution.EagerExecution();
+        //Console.WriteLine("--------------------------------------");
+        //queryExecution.ReuseQueryDefinition();
 
         #endregion
+
+
+        #region Projections
+
+        Projections projections = new Projections();
+        projections.SelectSyntax();
+        Console.WriteLine("--------------------------------------");
+        //projections.SelectProperty();
+        Console.WriteLine("--------------------------------------");
+        projections.TransformWithSelect();
+        Console.WriteLine("--------------------------------------");
+        projections.SelectByCaseAnonymous();
+        Console.WriteLine("--------------------------------------");
+        projections.SelectByCaseTuple();
+        Console.WriteLine("--------------------------------------");
+        projections.SelectAnonymousConstructions();
+        Console.WriteLine("---------------------------------------");
+        projections.SelectWithIndex();
+        Console.WriteLine("---------------------------------------");
+        //projections.IndexedSelectMany();
+        Console.WriteLine("--------------------------------------");
+
+        //// Here , there have more funactionality , see Projections class .
+
+        #endregion
+
+
+
     }
 }
