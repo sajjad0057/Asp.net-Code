6ae15841f4a735a11ff75c9fc1548db8d13c719b
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-18 23:50:42 +0600

    Practice BubbleSort Algo. using delegate feature

---
+++ FeaturePractice/DelegatesAndEvent_Practice/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvent_Practice
+{
+    public class BubbleSort<T>
+    {
+        public delegate int Compare(T item1, T item2);
+        public static void Sort(T[] items, Compare compare)
+        {
+            for (int j = 0; j <= items.Length - 2; j++)
+            {
+                for (int i = 0; i <= items.Length - 2; i++)
+                {
+                    if (compare(items[i], items[i + 1]) == -1)
+                    {
+                        var temp = items[i + 1];
+                        items[i + 1] = items[i];
+                        items[i] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
--- FeaturePractice/DelegatesAndEvent_Practice/Program.cs
+++ FeaturePractice/DelegatesAndEvent_Practice/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Delegate
+﻿using DelegatesAndEvent_Practice;
+
+namespace Delegate
 {
     public class Program
     {
@@ -28,6 +30,27 @@
         {
             Program.Calculate(1, 3, Print1);
             Program.Calculate(2, 3, Print2);
+
+            Console.WriteLine("Another Delegate Examples : ");
+
+            int CompareValues(int a, int b)
+            {
+                if(a>b)
+                    return -1;
+                else if(a<b)
+                    return 1;
+                return 0;
+
+            }
+
+            int[] a =new int[] {1,6,3,21,7,4,2,8,33};
+
+            BubbleSort<int>.Sort(a, CompareValues);
+
+            foreach (int i in a)
+            {
+                Console.Write($"{i} , ");
+            }
         }
     }
 }
