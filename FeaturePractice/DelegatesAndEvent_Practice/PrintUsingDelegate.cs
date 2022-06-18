455aff8205797dc65fa22c9c0f80de434a773e72
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-18 23:57:17 +0600

    rearrange delegate examples code

---
+++ FeaturePractice/DelegatesAndEvent_Practice/PrintUsingDelegate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvent_Practice
+{
+    public class PrintUsingDelegate
+    {
+        public delegate void PrintDelegate(object message);
+
+        public static void Calculate(int x, int y, PrintDelegate printer)
+        {
+            int result = x + y;
+            printer(result);
+        }
+    }
+}
--- FeaturePractice/DelegatesAndEvent_Practice/Program.cs
+++ FeaturePractice/DelegatesAndEvent_Practice/Program.cs
@@ -1,57 +1,39 @@
 ﻿using DelegatesAndEvent_Practice;
 
-namespace Delegate
+
+void Print1(object message)
 {
-    public class Program
-    {
-        public delegate void PrintDelegate(object message);
+Console.WriteLine(message);
+}
 
-        public static void Calculate(int x, int y, PrintDelegate printer)
-        {
-            int result = x + y;
-            printer(result);
-        }
-    }
+void Print2(object message)
+{
+Console.WriteLine($"-----{message}------");
+}
 
 
-    public class Test
-    {
-        public static void Print1(object message)
-        {
-            Console.WriteLine(message);
-        }
+PrintUsingDelegate.Calculate(1, 3, Print1);
+PrintUsingDelegate.Calculate(2, 3, Print2);
 
-        public static void Print2(object message)
-        {
-            Console.WriteLine($"-----{message}------");
-        }
+Console.WriteLine("Another Delegate Examples - Perform Bubble Sort : ");
 
-        public static void Main(string[] args)
-        {
-            Program.Calculate(1, 3, Print1);
-            Program.Calculate(2, 3, Print2);
 
-            Console.WriteLine("Another Delegate Examples : ");
+int CompareValues(int a, int b)
+{
+    if(a>b)
+        return -1;
+    else if(a<b)
+        return 1;
+    return 0;
 
-            int CompareValues(int a, int b)
-            {
-                if(a>b)
-                    return -1;
-                else if(a<b)
-                    return 1;
-                return 0;
+}
 
-            }
+int[] a =new int[] {1,6,3,21,7,4,2,8,33};
 
-            int[] a =new int[] {1,6,3,21,7,4,2,8,33};
+BubbleSort<int>.Sort(a, CompareValues);
 
-            BubbleSort<int>.Sort(a, CompareValues);
-
-            foreach (int i in a)
-            {
-                Console.Write($"{i} , ");
-            }
-        }
-    }
+foreach (int i in a)
+{
+    Console.Write($"{i} , ");
 }
 
