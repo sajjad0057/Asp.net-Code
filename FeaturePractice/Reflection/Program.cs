d1dbc49de78d8ebeb9ab858b0167b6961ac52013
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-19 15:55:36 +0600

    Finding class and  it's property a, then create a object using Reflection

---
+++ FeaturePractice/Reflection/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Reflection;
+using System.Reflection;
+
+Console.Write("Enter Class Name : ");
+string className = Console.ReadLine();
+
+Console.Write("Enter Property Name : ");
+string propertyName = Console.ReadLine();
+
+Console.Write("Enter Property Value : ");
+string propertyValue = Console.ReadLine();
+
+/*
+Returns
+Assembly
+The assembly that contains the code that is currently executing. 
+*/
+Assembly assembly = Assembly.GetExecutingAssembly();
+
+Type type = assembly.GetType(className);
+
+Console.WriteLine($"type : {type}");
+
+PropertyInfo property  = type.GetProperty(propertyName);
+
+
+/*
+set hare empty constructor , so parameter list is empty array . if Class contains 
+parameterized constructor , when add parameter type in Type array
+ */
+
+ConstructorInfo constructor = type.GetConstructor(new Type[] { });
+
+object instance = constructor.Invoke(new object[] { });
+
+property.SetValue(instance, propertyValue);
+
+
+Person person = instance as Person;
+
+Console.WriteLine(person.Name);
+
