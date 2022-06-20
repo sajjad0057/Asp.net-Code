55cbeb5b498e76bfe608c10fd32918b0e6f766e5
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-21 03:00:15 +0600

    Practice Recursive function

---
+++ FeaturePractice/TestCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int Recursion(int x)
+        {
+            if (x <= 1)
+                return 1;
+            else 
+                return x * Recursion(x - 1);
+        }
+
+        Console.WriteLine(Recursion(3));
+    }
+}
