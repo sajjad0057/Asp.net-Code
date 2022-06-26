fffa7028445e41b3b9124e7780379b41de8e1c6b
sajjad0057 <sajjadhossain0057@gmail.com>
2022-06-27 00:39:36 +0600

    practice reflection for solving assignment-1

--- FeaturePractice/TestCode/JsonFormatter.cs
+++ FeaturePractice/TestCode/JsonFormatter.cs
@@ -12,11 +12,16 @@ namespace TestCode
 
         public StringBuilder Json { get; set; }
 
+        public int ObjectPropertyLenghtFlag { get; set; }
+        public int ListPropertyLenghtFlag { get; set; }
+
         public JsonFormatter()
         {
             Json = new StringBuilder(string.Empty);
+            ObjectPropertyLenghtFlag = 0;
+            ListPropertyLenghtFlag = 0;
         }
-        public  string Convert(Object obj)
+        public string Convert(Object obj)
         {
             Type type = obj.GetType();
             if (!type.IsGenericType && !type.IsPrimitive)
@@ -24,6 +29,7 @@ namespace TestCode
                 Json.Append("{ \n");
             }
             PropertyInfo[] properties = type.GetProperties();
+            ObjectPropertyLenghtFlag = properties.Length;
             foreach (PropertyInfo property in properties)
             {
                 Type valueType = property.GetValue(obj).GetType();
@@ -32,34 +38,39 @@ namespace TestCode
                 {
                     if (valueType == typeof(string) || valueType.IsPrimitive)
                     {
-                        Json.Append($"\"{property.Name}\" : \"{property.GetValue(obj)}\",\n");
+                        Json.Append($"\"{property.Name}\" : \"{property.GetValue(obj)}\"");
+                        if (ObjectPropertyLenghtFlag > 1)
+                        {
+                            Json.Append(",\n");
+                        }
                         
                     }
                 }
                 else if(valueType.IsGenericType && valueType.GetGenericTypeDefinition() == typeof(List<>))
                 {
                     //Convert((IEnumerable<object>)property.GetValue(obj));
-                    Json.Append($"{property.Name} : [ \n");
+                    var list = (IEnumerable<object>)property.GetValue(obj);
+                    Json.Append($"{property.Name} : \n[");
                     foreach(var item in (IEnumerable<object>)property.GetValue(obj))
                     {
                         Convert(item);
                     }
-                    Json.Append(" ]\n");
+                    Json.Append("\n],\n");
                 }
                 else if(valueType.IsClass && valueType != typeof(string))
                 {
                     Convert(property.GetValue(obj));
                 }
+
+                ObjectPropertyLenghtFlag--;
                //Json.Append($"\"{ property.Name }\" : \"{property.GetValue(obj).GetType().IsGenericType}\",")
             }
 
-            Json.Append("}\n");
 
-            
+            Json.Append("\n}\n");
+                        
             return Json.ToString();
-
         }
-
 
 
     }
