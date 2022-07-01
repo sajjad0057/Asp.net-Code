31bc6f19de40713bd84a96569d6f762e0ddac0a3
sajjad0057 <sajjadhossain0057@gmail.com>
2022-07-01 16:46:48 +0600

    Practice Some New Features of C#-(11,10,9,8)

---
+++ FeaturePractice/CSharp_New_Feature/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_New_Feature
+{
+    public record Person1(string FirstName, string LastName);
+
+    public class Class1
+    {
+        public string Name { get; init; }
+
+
+
+    }
+
+
+    public struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Distance => Math.Sqrt(X * X + Y * Y);
+
+        public override string ToString() =>
+            $"({X}, {Y}) is {Distance} from the origin";
+    }
+
+
+
+    public class Class2
+    {
+        public enum Rainbow
+        {
+            Red,
+            Orange,
+            Yellow,
+            Green,
+            Blue,
+            Indigo,
+            Violet
+        }
+
+        public static string FromRainbow(Rainbow colorBand) =>
+             colorBand switch
+             {
+                 Rainbow.Red => "Color is red",
+                 Rainbow.Orange => "Color is Orange",
+                 Rainbow.Yellow => "Color is Yellow",
+                 Rainbow.Green => "Color is Green",
+                 Rainbow.Blue => "Color is Blue",
+                 Rainbow.Indigo => "Color is Indigo",
+                 Rainbow.Violet => "Color is Violet",
+                 _ => throw new ArgumentException(message: "invalid enum value", paramName: nameof(colorBand)),
+
+             };
+
+
+        public static async IAsyncEnumerable<int> GenerateSequence()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(100);
+                yield return i;
+            }
+        }
+
+
+
+    }
+   
+}
--- FeaturePractice/CSharp_New_Feature/Program.cs
+++ FeaturePractice/CSharp_New_Feature/Program.cs
@@ -1,73 +1,154 @@
 ﻿using CSharp_New_Feature;
 
 //// Feature of C# - 11 :
-
-
-
-int age = 25;
-string name = "sajjad";
-
-
-Console.WriteLine($"Hello," +
-    $"{name} -" +
-    $"You are {age} years old .");
-
-
-string longMessage = """
-    This is a long message.
-    It has several lines.
-        Some are indented
-                more than others.
-    Some should start at the first column.
-    Some have "quoted text" in them.
-    """;
-
-
-Console.WriteLine(longMessage);
-
-
-var location = $$"""
-   You are at {{{age}}, {{name}}}   
-   """;
-
-
-Console.WriteLine(location);
-
-//// OutPut :
-//// You are at {25, sajjad}
 ///
 
-
-///// declaring System.Console namespace globally and use WriteLine() only to print something instead of Console.WriteLine() . 
-
-WriteLine($"Use only WriteLine to print something , " +
-    $"for this using \"static System.Console\" namespace locally or glabally");
-
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
 
 
-WriteLine("---------------------------------------------------------------------");
+        int age = 25;
+        string name = "sajjad";
 
 
-Person person = new Person();
-
-//{ person.Location : new Address { City = "a", Street = "b" }; };
-
-
-WriteLine("--------------------------------------------------");
-////Improve tuple syntax : 
+        Console.WriteLine($"Hello," +
+            $"{name} -" +
+            $"You are {age} years old .");
 
 
-(int a, int b) point1 = (5, 8);
-
-(int x, int y) point;
-
-int x = 0;
-int y = 0;
-(x,  y) = point1;
-
-
-point = point1;
+       string longMessage = """
+        This is a long message.
+        It has several lines.
+            Some are indented
+                    more than others.
+        Some should start at the first column.
+        Some have "quoted text" in them.
+        """;
 
 
-WriteLine($"{point.Item1} ; {point.Item2} ");
+       Console.WriteLine(longMessage);
+
+
+      var location = $$"""
+       You are at {{{age}}, {{name}}}   
+       """;
+
+
+        Console.WriteLine(location);
+
+        //// OutPut :
+        //// You are at {25, sajjad}
+        ///
+
+
+        ///// declaring System.Console namespace globally and use WriteLine() only to print something instead of Console.WriteLine() . 
+
+        WriteLine($"Use only WriteLine to print something , " +
+            $"for this using \"static System.Console\" namespace locally or glabally");
+
+
+
+        WriteLine("---------------------------------------------------------------------");
+
+
+        Person person = new Person();
+
+        //{ person.Location : new Address { City = "a", Street = "b" }; };
+
+
+        WriteLine("--------------------------------------------------");
+        ////Improve tuple syntax : 
+
+
+        //(int a, int b) point1 = (5, 8);
+
+        //(int x, int y) point;
+
+        //int x = 0;
+        //int y = 0;
+        //(x,  y) = point1;
+
+
+        //point = point1;
+
+
+        //WriteLine($"{point.Item1} ; {point.Item2} ");
+
+
+        int M(int v)
+        {
+            int y = 5;
+            int x = 7;
+            return Add(x, y, v);
+
+            static int Add(int left, int right, int v) => left + right * v;
+        }
+
+
+        int r = M(4);
+
+        Console.WriteLine(r);
+
+
+        Class1 class1 = new Class1 { Name = "sajjad" };
+
+
+        Point point = new Point();
+
+
+        point.X = 1;
+        point.Y = 2;
+
+        WriteLine($"point Type : {point.GetType()}");
+        WriteLine($"{point.ToString()}");
+
+
+        WriteLine("<---------------- -------------------- -------------------- ------------------>");
+
+        WriteLine(Class2.FromRainbow(Class2.Rainbow.Red));
+
+
+
+        await foreach (var number in Class2.GenerateSequence())
+        {
+            Console.WriteLine(number);
+        }
+
+
+
+
+    var words = new string[]
+        {
+                        // index from start    index from end
+            "The",      // 0                   ^9
+            "quick",    // 1                   ^8
+            "brown",    // 2                   ^7
+            "fox",      // 3                   ^6
+            "jumped",   // 4                   ^5
+            "over",     // 5                   ^4
+            "the",      // 6                   ^3
+            "lazy",     // 7                   ^2
+            "dog"       // 8                   ^1
+        };              // 9 (or words.Length) ^0
+
+
+        Console.WriteLine("---------------------++++++----------------------");
+        Console.WriteLine($"The last word is {words[^1]}");
+        // writes "dog"
+        foreach (var word in words[6..])
+        {
+            Console.WriteLine(word);
+        }
+
+    }
+}
+
+
+
+
+
+
+
 
