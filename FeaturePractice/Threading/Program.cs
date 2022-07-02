5182328afdddd0e8917167bac747090bc85a0c86
sajjad0057 <sajjadhossain0057@gmail.com>
2022-07-03 03:37:52 +0600

    preactice threading (init)

---
+++ FeaturePractice/Threading/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+Thread thread1 = new Thread(() => Print1(100));   //// using this syntax , can pass a function with thread, otherwise need delegate features
+thread1.Start();
+
+Thread thread2 = new Thread(() => Print2(100));
+
+thread2.Start();
+
+
+void Print1(int value)
+{
+    for(int i = 0; i <= value; i++)
+    {   
+        if(i%2 != 0)
+            Console.WriteLine($"Odd : {i}");
+    } 
+}
+
+
+
+void Print2(int value)
+{
+    for(var i = 0; i <= value; i++)
+    {
+        if(i%2 == 0)
+            Console.WriteLine($"Even : {i}");
+    }
+}
+
+
+Console.WriteLine("+--------------->finish<---------------+");
+
+
+
