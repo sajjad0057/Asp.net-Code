bc7d7809e23541f782dcb5b6b6af9553694d4af7
sajjad0057 <sajjadhossain0057@gmail.com>
2022-07-04 01:24:43 +0600

    practice async program without await keyword (usign built in  method)

---
+++ FeaturePractice/Threading/AsyncExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    public class AsyncExample
+    {
+        public void Copy(string path)
+        {
+            ////File.ReadAllText() return only string , but File.ReadALlTextAsync() return a Task object .
+            
+            Task<string> task = File.ReadAllTextAsync(path);
+
+            task.Wait();     //// here wait for finishing task
+
+            string text = task.Result;
+
+        }
+
+    }
+}
